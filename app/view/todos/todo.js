1391bf8c2c744480aae17862632632c0e41b62f3
Arun Kumar <arun.b.kumar@unilever.com>
2019-03-18 19:35:29 +0530

    first commit

--- app/view/todos/todo.js
+++ app/view/todos/todo.js
@@ -1,6 +1,7 @@
 app.controller('todosCtrl', function ($scope, $http) {
     $http.get("json/todos.json")
         .then(function (response) {
+            //pooja
         $scope.todosData = response.data;
         });
 });
